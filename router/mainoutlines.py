aeef7e6d88707cefde2395b6e46a696e5529a6b8
MathxH Chen <brainfvck@foxmail.com>
2023-03-07 17:07:51 +0800

    Refactor

--- router/mainoutlines.py
+++ router/mainoutlines.py
@@ -7,6 +7,8 @@ from api import api
 
 ns = api.namespace("mainoutlines", description="TODO operations")
 
+main_outlines_list = list(feeds.global_main_outlines.keys())
+
 @ns.route("/")
 class MainOutlineList(Resource):
     """Shows a list of main outlines"""
@@ -15,7 +17,24 @@ class MainOutlineList(Resource):
         main_outlines = []
         for main_outline_title in feeds.global_main_outlines:
             main_outlines.append({
-            'index': list(feeds.global_main_outlines.keys()).index(main_outline_title),
+            'index': main_outlines_list.index(main_outline_title),
             'title': main_outline_title,
             })
         return main_outlines
+    
+@ns.route("/<int:index>")
+@api.doc(params={"index": "The main outline index the index returned from /mainoutlines"})
+class MainOutline(Resource):
+    def get(self, index):
+        """Shows a list sub outlines of the specific main outline"""
+        if index >= len(main_outlines_list):
+            api.abort(404, "Main outline {} doesn't exist".format(index))
+        main_outline_title = main_outlines_list[index]
+        suboutlines = feeds.global_main_outlines[main_outline_title]
+        sub_outlines = []
+        for sub_outline_title in suboutlines:
+            sub_outlines.append({
+                'title': sub_outline_title,
+                'xmlUrl': suboutlines[sub_outline_title].xmlUrl,
+            })
+        return sub_outlines
