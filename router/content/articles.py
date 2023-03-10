68098af89903482c9f865a7a4e8a5fb6eebb2ee4
MathxH Chen <brainfvck@foxmail.com>
2023-03-10 22:39:11 +0800

    Fix post parse error

--- router/content/articles.py
+++ router/content/articles.py
@@ -4,8 +4,8 @@ from api import api
 ns = api.namespace("content", description="Content Operations")
 
 parser = reqparse.RequestParser()
-parser.add_argument("title", type=str, required=True, help="The title of the article")
-parser.add_argument("content", type=str, required=True, help="The content of the article")
+parser.add_argument("title", type=str, required=True, help="The title of the article", location="json")
+parser.add_argument("content", type=str, required=True, help="The content of the article", location="json")
 
 resource_fields = api.model("Article", {
     'title': fields.String(required=True, description="The title of the article"),
@@ -15,10 +15,13 @@ resource_fields = api.model("Article", {
 @ns.route("/article")
 class Article(Resource):
     """Publish a new article from user"""
-   
-    @api.doc(body=resource_fields)
-    def post():
+    @ns.doc(parser=parser)
+    @ns.expect(parser)
+    @ns.marshal_with(resource_fields)
+    def post(self):
         args = parser.parse_args()
-        print ("totle: ", args['title'])
+        print ("totle: ", args["title"])
+        print ("content: ", args["content"])
+        return args, 200
         
 
